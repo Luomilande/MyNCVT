c91adf5b343371b397c449bfa6ddb31c8792f874
sbyhu@hotmail.com <ananda.1109>
2016-05-22 14:42:07 +0800

    5221441

--- MYNCVT.DAL/DALTeacherPosition.cs
+++ MYNCVT.DAL/DALTeacherPosition.cs
@@ -10,6 +10,10 @@ namespace MyNCVT.DAL
 {
     public class DALTeacherPosition
     {
+        /// <summary>
+        /// GetAllTeacherPosition():获取所有的教师职位
+        /// </summary>
+        /// <returns></returns>
         public IList<TeacherPosition> GetAllTeacherPosition()
         {
             IList<TeacherPosition> listTeacherPosition = new List<TeacherPosition>();
@@ -26,6 +30,24 @@ namespace MyNCVT.DAL
                 }
             }
             return listTeacherPosition;
+        }
+
+        /// <summary>
+        /// AddTeacherPosition(TeacherPosition teacherPosition)：添加一个教师职位记录
+        /// </summary>
+        /// <param name="teacherPosition"></param>
+        /// <returns></returns>
+        public bool AddTeacherPosition(TeacherPosition teacherPosition)
+        {
+            string sql = "insert into TeacherPosition(TeacherPositionName, TeacherPositionDescription) values(@TeacherPositionName, @TeacherPositionDescription)";
+            SqlParameter[] parameters = {
+                                            new SqlParameter("@TeacherPositionName", SqlDbType.VarChar, 50),
+                                            new SqlParameter("@TeacherPositionDescription", SqlDbType.VarChar, 200)
+                                        };
+            parameters[0].Value = teacherPosition.TeacherPositionName;
+            parameters[1].Value = teacherPosition.TeacherPositionDescription;
+            int n = DBHelper.ExecuteCommand(sql, parameters);
+            return n == 1;
         }
 
 
